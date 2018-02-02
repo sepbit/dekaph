90e8742f118a3dd2d6d88eb348277513ee98bf7a
Vitor Aloisio Guia <vitor.guia@vitortec.com>
2018-02-02 11:21:04 -0300

    Update DekatrianTest.php

--- tests/DekatrianTest.php
+++ tests/DekatrianTest.php
@@ -5,7 +5,7 @@
  * PHP Version 7.0
  *
  * @package   Vitorteccom\Dekaph
- * @author    Vitor Aloisio <vitor.guia@vitortec.com>
+ * @author    Vitor Guia <vitor.guia@vitortec.com>
  * @copyright 2011-2018 Vitortec.com
  * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
  * @version   GIT: <git_id>
@@ -21,7 +21,7 @@ use Vitorteccom\Dekaph\Dekatrian;
  * DekatrianTest class
  *
  * @package   Vitorteccom\Dekaph\DekatrianTest
- * @author    Vitor Aloisio <vitor.guia@vitortec.com>
+ * @author    Vitor Guia <vitor.guia@vitortec.com>
  * @copyright 2011-2018 Vitortec.com
  * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
  * @see       https://devs.vitortec.com/dekaph Documentation of Dekatrian Calendar
