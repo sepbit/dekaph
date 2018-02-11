0174b517171edd7e0370473769196d591cc48724
Vitor Guia <vitor.guia@vitortec.com>
2018-02-11 11:58:36 -0300

    Update Dekatrian.php

--- src/Dekatrian.php
+++ src/Dekatrian.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Vitortec.com Dekatrian Calendar
+ * Vitortec.com Dekaph
  *
  * PHP Version 7.0
  *
@@ -9,7 +9,7 @@
  * @copyright 2011-2018 Vitortec.com
  * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
  * @version   GIT: <git_id>
- * @see       https://devs.vitortec.com/dekaph Documentation of Dekatrian Calendar
+ * @see       https://devs.vitortec.com/dekaph Documentation of Dekaph
  */
 
 namespace Vitorteccom\Dekaph;
@@ -21,7 +21,7 @@ namespace Vitorteccom\Dekaph;
  * @author    Vitor Guia <vitor.guia@vitortec.com>
  * @copyright 2011-2018 Vitortec.com
  * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
- * @see       https://devs.vitortec.com/dekaph Documentation of Dekatrian Calendar
+ * @see       https://devs.vitortec.com/dekaph Documentation of Dekaph
  */
 class Dekatrian
 {
@@ -29,24 +29,24 @@ class Dekatrian
      * Check Dekatrian
      *
      * @param  integer $year  Dekatrian year
-     * @param  integer $mouth Dekatrian mouth
+     * @param  integer $month Dekatrian month
      * @param  integer $day   Dekatrian day
      *
      * @return boolean         Dekatrian date is valid or not
      */
-    public function checkDekatrian($year, $mouth, $day)
+    public function checkDekatrian($year, $month, $day)
     {
         if (date("L", mktime(0, 0, 0, 1, 1, $year))) {
-            if ($mouth == 0 && $day > 2) {
+            if ($month == 0 && $day > 2) {
                 return false;
             }
         } else {
-            if ($mouth == 0 && $day > 1) {
+            if ($month == 0 && $day > 1) {
                 return false;
             }
         }
 
-        if ($mouth > 13 || $day > 28) {
+        if ($month > 13 || $day > 28) {
             return false;
         }
 
@@ -57,18 +57,18 @@ class Dekatrian
      * Dekatrian to Gregorian
      *
      * @param  integer $year  Dekatrian year
-     * @param  integer $mouth Dekatrian mouth
+     * @param  integer $month Dekatrian month
      * @param  integer $day   Dekatrian day
      *
      * @return string         Gregorian date, e.g. 2018-12-31
      */
-    public function dekatoGreg($year, $mouth, $day)
+    public function dekatoGreg($year, $month, $day)
     {
-        if (!$this->checkDekatrian($year, $mouth, $day)) {
+        if (!$this->checkDekatrian($year, $month, $day)) {
             return false;
         }
 
-        $yearDay = ($mouth*28)-28;
+        $yearDay = ($month*28)-28;
         $yearDay = $yearDay+$day;
 
         //leap year
@@ -76,13 +76,13 @@ class Dekatrian
             $yearDay++;
 
             //Sinchronian day
-            if ($mouth == 0 && $day == 2) {
+            if ($month == 0 && $day == 2) {
                 return $year ."-1-2";
             }
         }
 
         //Achronian day
-        if ($mouth == 0 && $day == 1) {
+        if ($month == 0 && $day == 1) {
             return $year ."-1-1";
         }
 
@@ -93,43 +93,43 @@ class Dekatrian
      * Gregorian to Dekatrian
      *
      * @param  integer $year  Gregorian year
-     * @param  integer $mouth Gregorian mouth
+     * @param  integer $month Gregorian month
      * @param  integer $day   Gregorian day
      *
      * @return string        Dekatrian date, e.g. 2018-13-28
      */
-    public function gregToDeka($year, $mouth, $day)
+    public function gregToDeka($year, $month, $day)
     {
-        if (!checkdate($mouth, $day, $year)) {
+        if (!checkdate($month, $day, $year)) {
             return false;
         }
 
-        $yearDay = date("z", mktime(0, 0, 0, $mouth, $day, $year));
+        $yearDay = date("z", mktime(0, 0, 0, $month, $day, $year));
 
         //leap year
-        if (date("L", mktime(0, 0, 0, $mouth, $day, $year))) {
+        if (date("L", mktime(0, 0, 0, $month, $day, $year))) {
             $yearDay--;
 
             //Sinchronian day
-            if ($mouth == 1 && $day == 2) {
+            if ($month == 1 && $day == 2) {
                 return $year .'-0-2';
             }
         }
 
         //Achronian day
-        if ($mouth == 1 && $day == 1) {
+        if ($month == 1 && $day == 1) {
             return $year .'-0-1';
         }
 
-        $mouth = floor($yearDay/28);
-        $day = $yearDay-($mouth*28);
-        $mouth++;
+        $month = floor($yearDay/28);
+        $day = $yearDay-($month*28);
+        $month++;
 
         if (!$day) {
             $day = 28;
-            $mouth--;
+            $month--;
         }
 
-        return $year .'-'. $mouth .'-'. $day;
+        return $year .'-'. $month .'-'. $day;
     }
 }
